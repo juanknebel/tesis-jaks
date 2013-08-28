c8cd0bd6fa2758810924f5b7ddac35fb49b44b1d
juanknebel <juanknebel@gmail.com>
2013-08-28 00:15:13 -0300

    Nuevos tests

---
+++ src/test/testingClusterAndPickSolver.cpp
@@ -0,0 +1,40 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) 2013  Juan <email>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+
+#include "testingClusterAndPickSolver.h"
+
+TestingClusterAndPickSolver::TestingClusterAndPickSolver(ProblemInstance& problem) {
+	this->clusterSovler_ = new ClusterAndPickSolver(&problem);
+}
+
+TestingClusterAndPickSolver::TestingClusterAndPickSolver ( const TestingClusterAndPickSolver& other ) {
+}
+
+TestingClusterAndPickSolver::~TestingClusterAndPickSolver() {
+}
+
+void TestingClusterAndPickSolver::testingSolve ( int numFlakes ) {
+	SnowFlakeVector* result = this->clusterSovler_->solve(numFlakes);
+	for(SnowFlakeVector::iterator it = result->begin(); it != result->end(); ++it) {
+		it->showMe();
+	}
+}
+
+
