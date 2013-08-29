73f0c4a769ee04581633e21cb29260110b0477f1
Amit Stein <amit@linux-ryp5.site>
2013-08-28 21:35:20 -0300

    HAC with specific item

---
+++ src/solver/restrictedHACWithSpecificItemSolver.h
@@ -0,0 +1,26 @@
+/*
+ * RestrictedHACSWithSpecificItemSolver.h
+ *
+ *  Created on: 13/08/2013
+ *      Author: zero
+ */
+
+#ifndef RESTRICTEDHACSWITHSPECIFICITEMSOLVER_H_
+#define RESTRICTEDHACSWITHSPECIFICITEMSOLVER_H_
+
+#include "multiplicativeEffortProduceAndChooseSolver.h"
+
+class RestrictedHACSWithSpecificItemSolver: protected MultiplicativeEffortProduceAndChooseSolver {
+public:
+	RestrictedHACSWithSpecificItemSolver(ProblemInstance* problem, int specificItem) :  MultiplicativeEffortProduceAndChooseSolver(problem) {
+		this->_specificItem = specificItem;
+	}
+	virtual ~RestrictedHACSWithSpecificItemSolver();
+
+	SnowFlakeVector* produceManySnowflakes(int numToProduce);
+protected:
+	bool tryMerge(Int2ObjectOpenHashMap& clustering);
+	int _specificItem;
+};
+
+#endif /* RESTRICTEDHACSOLVER_H_ */
