5f28e1280896f019acc6910f4dd46c6be8131a1b
juanknebel <juanknebel@gmail.com>
2013-09-22 23:13:31 -0300

    El script de python para calcular el costo final de la solucion. Correcciones al executeSolver

--- src/executeSolver.cpp
+++ src/executeSolver.cpp
@@ -18,6 +18,11 @@ using namespace std;
 ProblemInstance* instanceTheProblem(ConfigurationJaks& configFile) {
 	std::string directory = configFile["directory_work"];
 	ProblemInstance* theProblem = new ProblemInstanceFromFiles(directory+configFile["file_costs"], directory+configFile["file_compat"], directory+configFile["file_cover"], atof(configFile["budget"].c_str()));
+	bool withSpecificItem = ((configFile["with_specific_item"] == "1") ? true : false);
+	int specificItem = atoi(configFile["specific_item"].c_str());
+	if (withSpecificItem) {
+		theProblem->setSpecificItem(specificItem);
+	}
 	return theProblem;
 }
 
@@ -33,16 +38,23 @@ void writeSolution(const SnowFlakeVector& solution, ConfigurationJaks& configFil
 	}
 }
 
+ProduceAndChooseSolver::RankingStrategy checkAndReturnStrategy(ConfigurationJaks& configFile) {
+	int similarity = atoi(configFile["inter_similarity_weight"].c_str());
+	int strategy = atoi(configFile["ranking_strategy"].c_str());
+	if (similarity == 1 && strategy == 0) {
+		std::cerr<<"De usar la INTER_SIMILARITY_WEIGHT, la estrategia debe ser #RANK_BY_INTRA_INTER o #RANK_BY_DENSEST_SUBGRAPH)"<<std::endl;
+	}
+	return static_cast<ProduceAndChooseSolver::RankingStrategy> (strategy);
+}
+
 void execute(ConfigurationJaks& configFile) {
 	int solverId = atoi(configFile["SOLVER"].c_str());
-	ProblemInstance *theProblem = instanceTheProblem(configFile);
+	ProblemInstance* theProblem = instanceTheProblem(configFile);
 	Id2Str* theNodeName = instanceTheNodeName(configFile);
-	bool withSpecificItem = ((configFile["with_specific_item"] == "1") ? true : false);
-	int specificItem = atoi(configFile["specific_item"].c_str());
-	if (withSpecificItem) {
-		theProblem->setSpecificItem(specificItem);
-	}
-	Solver * theSolver = 0;
+	ProduceAndChooseSolver::RankingStrategy strategy = checkAndReturnStrategy(configFile);
+	int numberOfSnowFlakes = atoi(configFile["num_flakes"].c_str());
+	Solver* theSolver = 0;
+	
 	switch(solverId) {
 		case ClusterAndPick:
 			std::cout<<"Ejecutando ClusterAndPickSolver ..."<<std::endl;
@@ -51,26 +63,32 @@ void execute(ConfigurationJaks& configFile) {
 		case RestrictedHAC:
 			std::cout<<"Ejecutando RestrictedHACSolver ..."<<std::endl;
 			theSolver = new RestrictedHACSolver(theProblem);
+			dynamic_cast<RestrictedHACSolver *> (theSolver)->setRankingStrategy(strategy);
 			break;
 		case RestrictedHACSpecific:
 			std::cout<<"Ejecutando RestrictedHACSpecificSolver ..."<<std::endl;
 			theSolver = new RestrictedHACSWithSpecificItemSolver(theProblem);
+			dynamic_cast<RestrictedHACSWithSpecificItemSolver *> (theSolver)->setRankingStrategy(strategy);
 			break;
 		case RandomBOBO:
 			std::cout<<"Ejecutando RandomBOBOSolver ..."<<std::endl;
 			theSolver = new RandomBOBOSolver(theProblem);
+			dynamic_cast<RandomBOBOSolver *> (theSolver)->setRankingStrategy(strategy);
 			break;
 		case RandomSOBO:
 			std::cout<<"Ejecutando RandomSOBOSolver ..."<<std::endl;
 			theSolver = new RandomSOBOSolver(theProblem);
+			dynamic_cast<RandomSOBOSolver *> (theSolver)->setRankingStrategy(strategy);
 			break;
 		case ExAnySimSOBO:
 			std::cout<<"Ejecutando ExhaustiveGreedyAnySimSolver ..."<<std::endl;
 			theSolver = new ExhaustiveGreedyAnySimSOBOSolver(theProblem);
+			dynamic_cast<ExhaustiveGreedyAnySimSOBOSolver *> (theSolver)->setRankingStrategy(strategy);
 			break;
 		case ExSumSimSOBO:
 			std::cout<<"Ejecutando ExhaustiveGreedySumSimSolver ..."<<std::endl;
 			theSolver = new ExhaustiveGreedySumSimSOBOSolver(theProblem);
+			dynamic_cast<ExhaustiveGreedySumSimSOBOSolver *> (theSolver)->setRankingStrategy(strategy);
 			break;
 		case SeqScan:
 			std::cout<<"Ejecutando SequentialScanSolver ..."<<std::endl;
@@ -80,7 +98,7 @@ void execute(ConfigurationJaks& configFile) {
 			std::cerr<<"El metodo elegido para la resolucion no existe"<<std::endl;
 			break;
 	}
-	SnowFlakeVector* solution = theSolver->solve(atoi(configFile["num_flakes"].c_str()));
+	SnowFlakeVector* solution = theSolver->solve(numberOfSnowFlakes);
 	cout<<SnowFlake::showSolution(*solution,theNodeName)<<endl;
 	writeSolution(*solution, configFile);
 	delete solution;
