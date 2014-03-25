5c068d5785beb97d6eb517cd357cce6a25833fca
Juan Knebel <juanknebel@gmail.com>
2014-03-25 00:55:25 -0300

    nuevo algoritmo de clusterizacion jerarquico

--- src/solver/restrictedHACSolver.cpp
+++ src/solver/restrictedHACSolver.cpp
@@ -17,70 +17,119 @@ SnowFlakeVector* RestrictedHACSolver::produceManySnowflakes(int numToProduce) {
 	}
 	// Put each item in its own cluster
     MapIntIntSet *clustering = new MapIntIntSet();
-	IntSet ids = this->problem_->getIds();
-	for (IntSet::iterator it = ids.begin(); it != ids.end(); ++it) {
-		// Make sure all singleton clusters are within budget
-		if (this->problem_->getCost(*it) <= this->problem_->getbudget()) {
-			IntSet *temp = new IntSet();
-			temp->insert(*it);
-			(*clustering)[*it] = temp;
-		}
-	}
-	// Merge iteratively
-	bool merged = true;
-	while(merged && clustering->size() > numToProduce) {
-		merged = tryMerge(clustering);
-	}
+    int totalElements = this->problem_->numNodes();
 
-	if( merged == false ) {
-		DEBUG(DBG_DEBUG,"Could not merge more, stopped at "<<clustering->size());
-	}
+    matrix<TupleIntDouble> *theMatrixC = new matrix<TupleIntDouble> (totalElements, totalElements);
+    IntVector *theIVector = new IntVector;
+    TupleIntDoubleVector *nbm = new TupleIntDoubleVector;
+    Double tempMaxSimilarity, similarity;
+    int tempMaxIndex;
+    //Inicializo el cluster, la matriz con las similitudes, vector de indices y nbm
+    for (int i = 0; i < totalElements; ++i) {
+        //init del clustering
+        IntSet *temp = new IntSet();
+        temp->insert(i);
+        (*clustering)[i] = temp;
+        //init de matriz y vectores
+        tempMaxSimilarity = -1.00;
+        tempMaxIndex = -1;
+        for (int j = 0; j < totalElements; ++j) {
+            similarity = this->problem_->getCompat(i, j);
+            if (similarity > tempMaxSimilarity && i != j) {
+                tempMaxIndex = j;
+                tempMaxSimilarity = similarity;
+            }
+            TupleIntDouble aValue(j, similarity);
+            theMatrixC->insert_element(i, j, aValue);
+        }
+        theIVector->push_back(i);
+        TupleIntDouble otherValue (tempMaxIndex, tempMaxSimilarity);
+        nbm->push_back(otherValue);
+    }
+
+    //Empiezo a intentar clusterizar
+    int j = 0;
+    while (j < totalElements - 1 && clustering->size() > numToProduce) {
+        DEBUG(DBG_DEBUG,"tamanio del cluster " << clustering->size());
+        tempMaxSimilarity = -1.00;
+        int anElementi1, otherElementi2;
+        //Busco en el nbm el elemento que contenga la mayor similitud para algun otro elemento
+        DEBUG(DBG_DEBUG,"Busco en el nbm el elemento que contenga la mayor similitud para algun otro elemento");
+        for (int k = 0; k < totalElements; ++k) {
+            TupleIntDouble aTupleToCompare = nbm->at(k);
+            if (std::get<1>(aTupleToCompare) > tempMaxSimilarity && k == theIVector->at(k)) {
+                anElementi1 = k;
+                tempMaxSimilarity = std::get<1>(aTupleToCompare);
+            }
+        }
+
+        //busco recursivamente el segundo elemento
+        int theIndex = std::get<0>(nbm->at(anElementi1));
+        bool notIndex = true;
+        while (notIndex == true) {
+            if (theIVector->at(theIndex) == theIndex) {
+                notIndex = false;
+            }
+            else {
+                theIndex = theIVector->at(theIndex);
+            }
+        }
+        otherElementi2 = theIVector->at(theIndex);
+
+        //copio elementos al cluster correspondiente
+        DEBUG(DBG_DEBUG,"copio elementos al cluster correspondiente");
+        DEBUG(DBG_DEBUG,"i1 "<<anElementi1);
+        IntSet *besti1 = clustering->at(anElementi1);
+        DEBUG(DBG_DEBUG,"i2 "<<otherElementi2);
+        IntSet *besti2 = clustering->at(otherElementi2);
+        besti1->insert(besti2->begin(), besti2->end());
+        clustering->erase(otherElementi2);
+
+        //actualizacion del vector de indices y de la matriz de similitud
+        DEBUG(DBG_DEBUG,"actualizacion del vector de indices y de la matriz de similitud");
+        for (int i = 0; i < totalElements; ++i) {
+            if (theIVector->at(i) == i && i != anElementi1 && i != otherElementi2) {
+
+                TupleIntDouble aTupleToCompare = (*theMatrixC)(anElementi1, i);
+                TupleIntDouble otherTupleToCompare = (*theMatrixC)(otherElementi2, i);
+                int index = std::get<0>((*theMatrixC)(anElementi1, i));
+                Double maxSimilarity = (std::get<1>(aTupleToCompare) > std::get<1>(otherTupleToCompare)) ? std::get<1>(aTupleToCompare) : std::get<1>(otherTupleToCompare);
+                theMatrixC->insert_element(anElementi1, i, TupleIntDouble(index, maxSimilarity));
+                theMatrixC->insert_element(i, anElementi1, TupleIntDouble(index, maxSimilarity));
+
+            }
+            if (theIVector->at(i) == otherElementi2) {
+                (*theIVector)[i] = anElementi1;
+            }
+        }
+        //actualizo nbm
+        DEBUG(DBG_DEBUG,"actualizo nbm");
+        tempMaxSimilarity = -1.00;
+        for (int i = 0; i < totalElements; ++i) {
+            TupleIntDouble tempTuple = (*theMatrixC)(anElementi1, i);
+            if (std::get<1>(tempTuple) > tempMaxSimilarity && i != anElementi1 && theIVector->at(i) == i) {
+                tempMaxSimilarity = std::get<1>(tempTuple);
+                //(*nbm)[anElementi1] = tempTuple;
+                (*nbm)[anElementi1] = TupleIntDouble (i, tempMaxSimilarity);
+            }
+        }
+        ++j;
+    }
+
 	SnowFlakeVector* solution = new SnowFlakeVector;
     for (MapIntIntSet::iterator it = clustering->begin(); it != clustering->end(); ++it) {
 		SnowFlake *aFlake = new SnowFlake(*it->second, this->problem_);
 		solution->push_back(*aFlake);
 	}
+
+    delete theIVector;
+    delete theMatrixC;
+    delete nbm;
 	delete clustering;
 
 	return solution;
 }
 
-bool RestrictedHACSolver::tryMerge(MapIntIntSet* clustering) {
-	// Compute all distances
-	int bestC1 = -1;
-	int bestC2 = -1;
-	Double maxCompatibility = -1.00;
-    for (MapIntIntSet::iterator it = clustering->begin(); it != clustering->end(); ++it) {
-		IntSet *cluster1 = it->second;
-        for (MapIntIntSet::iterator it2 = it; it2 != clustering->end(); ++it2) {
-			if (it == it2) {
-				continue;
-			}
-			IntSet *cluster2 = it2->second;
-			//check if these can be merged
-			if (this->checkBudgetAndCoverageConstraint(*cluster1, *cluster2)) {
-				// if they can be merged, measure their compatibility
-				Double compatibility = this->problem_->maxPairwiseCompatibility(*cluster1, *cluster2);
-				if (compatibility > maxCompatibility) {
-					bestC1 = it->first;
-					bestC2 = it2->first;
-					maxCompatibility = compatibility;
-				}
-			}
-		}
-	}
+void RestrictedHACSolver::singleLinkClustering(MapIntIntSet* clustering) {
 
-	if (bestC1 >= 0 && bestC2 >= 0) {
-		//copy elements from c2 into c1
-		IntSet *bestSetC1 = clustering->at(bestC1);
-		IntSet *bestSetC2 = clustering->at(bestC2);
-		bestSetC1->insert(bestSetC2->begin(), bestSetC2->end());
-
-		//remove c2 from cluster
-		clustering->erase(bestC2);
-		return true;
-	}
-	else {
-		return false;
-	}
 }
